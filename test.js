8066f499ee6e4abf50c8fb95221a314718896d34
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-06 00:19:00 +0500

    Binding "this"
    ---- Binding "this" to a permanent value ----
    
    - Function Binding helps us permanently fix the value of "this"
    
    - Advantage/Example in the code

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
-
-const person = {
+let person = {
     name: 'Ammar',
     walk() {
-        console.log(this);
-    }    
+        // to use the Template variables, use ` (tilde key) instead of '
+        console.log(`hey!, ${this.name}`);
+    }
 };
-
-// the value of this is determined by how a function is called, 
-// if we call a function as a method in an object,
-// "this" will always reutrn a reference to that object.
-
-// if we call a function as a standalone object, or outside an object "this" will
-// return the global object which is: the window object in browsers.
-person.walk();
-
-const walk = person.walk; //Just getting a reference to the walk() function. (NOT CALLING IT)
-walk();
+let boundPersonWalk = person.walk.bind(person); // Binding the function.
+setTimeout(boundPersonWalk, 1000); // prints "hey!, Ammar"
+// If we hadn't bound the function, it would print "hey!, " losing "this".
