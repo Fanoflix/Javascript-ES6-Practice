df0ecc4cbbc747022bef30a12a7acd5c166da74d
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-05 19:59:57 +0500

    Variable Type: const
    ----- const -----
    - const keyword is also scoped to the block
     > const variables are constant, cannot be reassigned.

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 // var keyword is scoped to the function (annoying)
 // let keyword is scoped to the block (ez)
+// const keyword is also scoped to the block
+// > const variables are constant, cannot be reassigned.
 function hey()
 {
     for (let i = 0 ; i < 5 ; i++)
