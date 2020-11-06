55763f9d8b27c89b05d824655a23a3cee82d1196
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-06 22:26:41 +0500

    Spread Operator with Objects
    --- Spread Operator with Objects ---
    
    - NOTE: We use [] for using spread operator with arrays.
    - NOTE: We use {} for using spread operator with objects.

--- test.js
+++ test.js
@@ -1,27 +1,15 @@
-// --- SPREAD OPERATOR ---
-const first = [1,2,3];
-const second = [4,5,6];
+// SPREAD OPERATOR ON OBJECTS
 
-// spread operator --> ...;
-// the triple dots mean including all the content of the array
-// ...first means all the elements of the array 'first'
+const first = { name: 'Ammar'};
+const second = { job: 'Student'};
 
-// concatinating two arrays
-const combined = [...first, ...second];
-console.log(combined);
+const combined = {...first, ...second, location: 'Pakistan'};
+console.log(combined); // {name: "Ammar", job: "Student", location: "Pakistan"}
 
-// useful for adding values in the middle
-const combined2 = [...first, 'a', ...second]; 
-console.log(combined2); // [1, 2, 3, "a", 4, 5, 6]
 
-// some more examples:
-const combined3 = ['a', ...first, ...second]; 
-const combined4 = [...first, ...second, 'a'];
-console.log(combined3); // ["a", 1, 2, 3, 4, 5, 6]
-console.log(combined4); // [1, 2, 3, 4, 5, 6, "a"]
+// cloning objects
+// NOTE: We use [] for using spread operator with arrays.
+// NOTE: We use {} for using spread operator with objects.
 
-// cloning arrays
+const newObj = {...first};
 
-const close = [...first];
-
-// spread operator on objects
